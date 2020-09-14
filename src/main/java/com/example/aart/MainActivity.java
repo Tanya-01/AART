a557c68256d43a88010a1cc480f21399fbc290b1
Tanya Thakur <tanyathakur2000@gmail.com>
2020-09-14 17:57:18 +0530

    Testing change

--- src/main/java/com/example/aart/MainActivity.java
+++ src/main/java/com/example/aart/MainActivity.java
@@ -3,12 +3,13 @@ package com.example.aart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-aart 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity {
+public void()
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
